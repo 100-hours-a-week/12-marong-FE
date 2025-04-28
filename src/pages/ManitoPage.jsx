3856a2956cb345a24533df34ebb3c0b88ffe37d4
kwakyongwoo <kkolbuyw@gmail.com>
2025-04-28 20:28:57 +0900

    Feat: 기본 화면 생성 및 네이게이션 설정 (#2)

---
+++ src/pages/ManitoPage.jsx
@@ -0,0 +1,7 @@
+export default function ManitoPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold">마니또</h1>
+    </div>
+  )
+}
