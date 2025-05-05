27e9abc0d35690f971045c850fe592977b335fa8
kwakyongwoo <kkolbuyw@gmail.com>
2025-05-05 18:20:01 +0900

    Feat: 설문 기본 컴포넌트 추가 (#4) - 설문 진행률 및 헤더

---
+++ src/components/HorizontalProgressBar.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+function HorizontalProgressBar({ progress }) {
+  return (
+    <div className="w-full bg-gray-100 rounded-full h-3">
+      <div
+        className="bg-brand-pink h-3 rounded-full"
+        style={{ width: `${progress}%` }}
+      ></div>
+    </div>
+  );
+}
+
+export default HorizontalProgressBar;
