26636381798fe519a15e0b23f728e473da8ef816
kwakyongwoo <kkolbuyw@gmail.com>
2025-05-12 17:20:49 +0900

    Feat: 피드 화면 무한스크롤 기능 구현 (#21)

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,28 +1,39 @@
 import {useEffect, useState} from "react";
-import MainAppBar from "../components/MainAppBar.jsx";
 import api from "../api/axios.js";
 import FeedCard from "../components/FeedCard.jsx";
 import {useGroup} from "../context/GroupContext.jsx";
 import FloatingAddButton from "../components/FloatingAddButton.jsx";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function Feed() {
-  const { selectedGroup } = useGroup()
+  const {selectedGroup} = useGroup()
   const [feeds, setFeeds] = useState([])
+  const [page, setPage] = useState(1)
 
-  useEffect(() => {
-    console.log("Feeds: ", feeds)
-  }, [feeds])
-
-  useEffect(() => {
-    api.get("feeds")
+  const getFeeds = () => {
+    api.get("feeds", {
+      params: {
+        page: page,
+      },
+    })
       .then(res => {
         console.log(res.data.data)
         const feedsWithLike = res.data.data.feeds.map((feed) => ({
           ...feed, isLiked: true
         }))
 
-        setFeeds(feedsWithLike)
+        setFeeds(feeds.concat(feedsWithLike))
+
+        setPage(page + 1)
       })
+  }
+
+  useEffect(() => {
+    console.log("Feeds: ", feeds)
+  }, [feeds])
+
+  useEffect(() => {
+    getFeeds()
   }, [])
 
   const toggleLike = (feedId) => {
@@ -54,14 +65,23 @@ function Feed() {
   return (
     <div className="flex flex-col px-4">
       {/* 피드 목록 */}
-      <div className="flex-1 overflow-y-auto">
+      <InfiniteScroll
+        className="flex-1 overflow-y-auto"
+        dataLength={feeds.length}
+        next={getFeeds}
+        hasMore={true}
+        loader={<h4 className="text-center">Loading...</h4>}
+      >
         {feeds.map((feed) => (
           <FeedCard feed={feed} setLike={toggleLike} key={feed.feedId}/>
         ))}
-      </div>
+      </InfiniteScroll>
+
+      {/*<div className="flex-1 overflow-y-auto">*/}
+
 
       {/* 피드 작성 버튼 */}
-      <FloatingAddButton onClick={addPost} />
+      <FloatingAddButton onClick={addPost}/>
     </div>
   )
 }
